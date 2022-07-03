ce0aec30d691e06a391204db2f8829faf024bde2
bonzar <nva112002@gmail.com>
2022-07-04 00:00:48 +0300

    Change <img> to <picture> with screen size condition to choose from portrait for mobile and landscape for desktop.

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,19 @@
-var myImage = document.querySelector('img');
+var myPicture = document.querySelector('picture');
+var myPicSourcesPortaint = myPicture.querySelector('source[media="(max-width: 768px)"]');
+var myPicSourcesLandscape = myPicture.querySelector('source[media="(min-width: 769px)"]');
+var myImage = myPicture.querySelector('img');
 
 myImage.onclick = function () {
-    var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/хай единорог 203003.jpeg') {
-        myImage.setAttribute('src', 'images/золотая рыбка ххх.jpg');
+    var mySrc = myPicSourcesLandscape.getAttribute('srcset');
+    if(mySrc === 'images/vlad-wide-landscape.jpeg') {
+        myPicSourcesPortaint.setAttribute('srcset', 'images/Oddi-close-portrait.jpeg');
+        myPicSourcesLandscape.setAttribute('srcset', 'images/Oddi-wide-landscape.jpeg');
+        myImage.setAttribute('src', 'images/Oddi-wide-landscape.jpeg');
+
     } else {
-        myImage.setAttribute('src', 'images/хай единорог 203003.jpeg');
+        myPicSourcesPortaint.setAttribute('srcset', 'images/vlad-close-portrait.jpeg');
+        myPicSourcesLandscape.setAttribute('srcset', 'images/vlad-wide-landscape.jpeg');
+        myImage.setAttribute('src', 'images/vlad-wide-landscape.jpeg');
     }
 }
 var myButton = document.querySelector('button');
