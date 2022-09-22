da61134f4a26ed1d457caf88b835ac870991d8df
bonzar <nva112002@gmail.com>
2022-09-22 12:08:16 +0300

    add parcel, prettier, eslint, babel to project

--- projects/marketplaces-stocks/scripts/login.js
+++ projects/marketplaces-stocks/scripts/login.js
@@ -1,19 +1,19 @@
 export function getOzonClientId(isNewId = false) {
-    let clientId = localStorage.getItem('clientId');
-    if (!clientId || isNewId) {
-        clientId = prompt('Enter client id: ');
-        localStorage.setItem('clientId', clientId);
-    }
+  let clientId = localStorage.getItem("clientId");
+  if (!clientId || isNewId) {
+    clientId = prompt("Enter client id: ");
+    localStorage.setItem("clientId", clientId);
+  }
 
-    return clientId;
+  return clientId;
 }
 
 export function getOzonApiKey(isNewKey = false) {
-    let apiKey = localStorage.getItem('ApiKey');
-    if (!apiKey || isNewKey) {
-        apiKey = prompt('Enter API key: ');
-        localStorage.setItem('ApiKey', apiKey);
-    }
+  let apiKey = localStorage.getItem("ApiKey");
+  if (!apiKey || isNewKey) {
+    apiKey = prompt("Enter API key: ");
+    localStorage.setItem("ApiKey", apiKey);
+  }
 
-    return apiKey;
+  return apiKey;
 }
